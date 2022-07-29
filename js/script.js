6340628796dd205e6f71ace7223fc9d468203e9a
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-29 09:57:52 -0500

    clean-up

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-let buttons = document.querySelector('buttons')
-let inputs = document.querySelector('input')
+let buttons = document.querySelector('.buttons')
+let input = document.querySelector('.input')
+
+let operators = document.querySelector('.operators')
+let numbers = document.querySelector('.numbers')
+
+// let operators = document.querySelctor('#operators')
 
 
-inputs.addEventListener('click', e=>{
-  console.log(e)
+numbers.addEventListener('click', e =>{
+  // console.log(e)
+
+  let onClick = e.target.innerText
+    console.log(onClick)
+
+    // if (onClick.target.length-1 == 'C')[
+
+    // ]
 
 })
+
+// buttons.addEventListener('click', e =>{
+//   let onClick = e.target.innerText 
+//     console.log(onClick)
+// }) 
